776482d4c8b211f27686ff03c052b66f7dcfea5f
René Senpai <sml@cin.ufpe.br>
2020-11-25 13:05:36 -0300

    Fix service file name

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const keystone = require('keystone');
 const cors = require('cors');
 
 const postController = require('../controllers/postController.js');
-const postService = require('../controllers/postServices.js');
+const serviceController = require('../controllers/serviceController.js');
 const bannerController  = require('../controllers/bannerController'); 
 
 module.exports = (app) => {
@@ -14,7 +14,7 @@ module.exports = (app) => {
   });
 
   app.get('/api/posts', postController.getPostController);
-  app.get('/api/services',postService.getServiceController);
+  app.get('/api/services',serviceController.getServiceController);
   app.get('/api/banners', bannerController.getBannerController);
 
   app.get('*', (req, res) => {
