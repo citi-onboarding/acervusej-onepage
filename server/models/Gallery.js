992ae1db87298046813357f266b9f7c5f8863d5c
Igor Phillipe <igrphillipe@gmail.com>
2020-11-25 15:35:40 -0300

    feat(gallery-flow): creat gallery model

---
+++ server/models/Gallery.js
@@ -0,0 +1,35 @@
+const keystone = require('keystone');
+const { Types } = keystone.Field;
+
+const Gallery = new keystone.List('Gallery', {
+    map: { name: 'name' },
+    label: 'Galeria',
+});
+
+Gallery.add({
+    title: {
+        type: Types.Text,
+        required: true,
+        initial: true,
+        label: 'Título',
+        note: 'Título do post'
+    },
+
+    image: {
+        type: Types.CloudinaryImage,
+        required: true,
+        initial: true,
+        label: 'Imagem'
+    },
+
+    description: {
+        type: Types.Textarea,
+        requeired: true;
+        initial: true,
+        label: 'Descrição',
+        note: 'Descrição sobreo post'
+    }
+
+});
+
+Gallery.register();
