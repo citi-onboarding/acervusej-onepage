fed040ab7328cc2661820e9b7a788403dd45bfc6
René Senpai <sml@cin.ufpe.br>
2020-12-09 19:57:34 -0300

    feat(modal): Make the strucuture of the modaland connect it with the OurService section

---
+++ client/src/components/Modal/Modal.jsx
@@ -0,0 +1,21 @@
+import { React } from 'react';
+import "./Modal.css";
+
+function Modal({image, title, desc, modalServiceVisible, func}){
+
+    return(
+        <div className = {modalServiceVisible? "modalOpened":"modalClosed"}>
+            <div className = "backgroundModal">
+                <button className = "buttonModal" onClick = {() => func()}></button>
+                <div className = "informationsModal">
+                    <img src = {image} className = "imgModal"/>
+                    <h1 className = "titleModal">{title}</h1>
+                    <p className= "descModal">{desc}</p>
+                </div>
+            </div>
+        </div>
+    )
+    
+}
+
+export default Modal;
--- client/src/components/OurServices/OurServices.jsx
+++ client/src/components/OurServices/OurServices.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect, Component } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Service from './Service/Service';
 import Slider from 'react-slick';
+import Modal from '../Modal/Modal';
 
 import './OurServices.css';
 
@@ -27,12 +28,30 @@ function OurServices() {
       speed: 500
   };
 
+  const [modalServiceVisible, setModalServiceVisible] = useState(false);
+  const [imgSrc, setImgSrc] = useState("");
+  const [titleSrc, setTitleSrc] = useState("");
+  const [descSrc, setDescSrc] = useState("");
+
+
+  const openModal = async (img, title, desc) =>{
+    setImgSrc(img);
+    setTitleSrc(title);
+    setDescSrc(desc);
+    setModalServiceVisible(!modalServiceVisible);
+  }
+
+  const closeModal = () =>{
+    setModalServiceVisible(false)
+  }
+
   return (
     <div className="all-services">
+      <Modal image = {imgSrc} title = {titleSrc} desc ={descSrc} modalServiceVisible ={modalServiceVisible} func={closeModal}/>
       <h1 className = "title-our-services">Nossos Serviços</h1>
       <Slider {...settings}>
         {services?.map(({ _id, image ,title, description}) =>(
-          <div key={_id} className="service-card">
+          <div key={_id} className="service-card" onClick= {() => openModal(image[0].secure_url, title, description)}>
             <Service  image = {image[0].secure_url} title= {title} desc = {description}/>
           </div>
         ))}
