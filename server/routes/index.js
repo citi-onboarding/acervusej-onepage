a9760d56a02478edb1cb9af096be9cb4c095c1ba
René Senpai <sml@cin.ufpe.br>
2020-12-11 13:41:10 -0300

    fix(finalchanges):Change the index.js do routes/server

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ const path = require('path');
 const keystone = require('keystone');
 const cors = require('cors');
 
-const postController = require('../controllers/postController.js');
 const aboutUsController = require('../controllers/aboutUsController');
 const serviceController = require('../controllers/serviceController.js');
 const bannerController = require('../controllers/bannerController');
@@ -17,7 +16,6 @@ module.exports = (app) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 
-    app.get('/api/posts', postController.getPostController);
     app.get('/api/about-us', aboutUsController.getAboutUsController);
     app.get('/api/services', serviceController.getServiceController);
     app.get('/api/banners', bannerController.getBannerController);
